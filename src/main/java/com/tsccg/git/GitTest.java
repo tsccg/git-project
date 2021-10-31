c183fe850d2fc52725fa3c6f0787dbbb85950051
TSCCG <2387600954@qq.com>
2021-10-31 16:48:35 +0800

    idea second commit

--- src/main/java/com/tsccg/git/GitTest.java
+++ src/main/java/com/tsccg/git/GitTest.java
@@ -7,5 +7,6 @@ package com.tsccg.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!2");
     }
 }
